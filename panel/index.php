be8eaef1dd75c6a3fabbf4b6e5fe5b6d644040bc
eliabe <eliabepaz.work@gmail.com>
2021-11-19 11:47:07 -0300

    qr_code/upload

--- panel/index.php
+++ panel/index.php
@@ -3,20 +3,32 @@ session_start();
 include("../functions/connection.php");
 include("../functions/fun.php");
 
+if(!empty($_SESSION['usuario']['id_usuario']))
+{}
+else{$_SESSION['msg']='Você precisa logar para acessar o painel!</br>';
+    header("Location: index.php");
+} 
+   
 if(isset($_SESSION['msg'])){
-    msg_sistem($_SESSION['msg']);
-    unset($_SESSION['msg']);
-  }
+  msg_sistem($_SESSION['msg']);
+  unset($_SESSION['msg']);
+}
+  
 
-
+var_dump($_SESSION);
 var_dump($_GET);
 
 echo("Usuario Logado: ".$_SESSION['usuario']['login']);
 
-
-
-echo("<br><a href='?sair=sim'>Sair</a>");  
-
+?>
+<a href='?sair=sim'>Sair</a>");  
+<br>
+<a class="text-decoration-none text-reset" href="cadastro.php">
+    <button type="button" class="btn btn-danger">
+        Cadastrar Item
+    </button>
+</a>
+<?php
 
 //logoff da sistema
 if($_GET){
