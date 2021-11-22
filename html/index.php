a4366777b82efab14be4460f259b454ff90c2584
Eliabe G da Paz <59834090+eliabeguerreiro@users.noreply.github.com>
2021-11-22 13:56:28 -0300

    Update and rename index.html to index.php

--- html/index.php
+++ html/index.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+include("../functions/connection.php");
+include("../functions/fun.php");
+
+if(!empty($_SESSION['usuario']['id_usuario']))
+{}
+else{$_SESSION['msg']='Você precisa logar para acessar o painel!</br>';
+    header("Location: index.php");
+} 
+   
+if(isset($_SESSION['msg'])){
+  msg_sistem($_SESSION['msg']);
+  unset($_SESSION['msg']);
+}
+/*
+echo("<div class='jumbotron'>");
+var_dump($_SESSION['usuario']);
+echo("</div><br>");
+echo("Usuario Logado: ".$_SESSION['usuario']['login']);
+*/
+
+if(!$_SESSION['usuario']['nome']){
+echo("<div class='jumbotron'>");
+
+
+
+echo("Usuario sem nome, atualize seus dados!</div><br>");
+ 
+}
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,8 +64,8 @@
     
             <ul class="list-unstyled components">
                 <form class="d-flex" id="searchbar">
-                    <input class="form-control me-2" type="search" placeholder="Procurar" aria-label="Search">
-                    <button class="btn btn-redeph-search busca-btn" type="submit">
+                    <input class="form-control me-2" name='id_pesquisa' type="search" placeholder="Procurar" aria-label="Search">
+                    <button class="btn btn-redeph-search busca-btn" name='SendPesqItem' type="submit">
                         <!-- Icone do Google Icons -->
                         <span class="material-icons">search</span>
                     </button>
@@ -42,13 +74,13 @@
                 
             <ul class="list-unstyled components" id="sidebar-links">
                 <li>
-                    <a href="./index.html">Início</a>
+                    <a href="../index.html">Início</a>
                 </li>
                 <li>
-                    <a href="./cadastro.html">Cadastro</a>
+                    <a href="cadastro.php">Cadastro</a>
                 </li>
                 <li>
-                    <a href="./scan.html">Escanear</a>
+                    <a href="#">Escanear</a>
                 </li>
                 <br>
             </ul>
@@ -100,3 +132,34 @@
 </script>
 </body>
 </html>
+  
+ <?php
+  
+  
+  $SendPesqItem = filter_input(INPUT_POST, 'SendPesqItem', FILTER_SANITIZE_STRING);
+
+if($SendPesqItem){
+
+  $pesquisa = filter_input(INPUT_POST, 'id_pesquisa', FILTER_SANITIZE_STRING);
+  $result_pesquisa = "SELECT * FROM itens WHERE id_item LIKE '%$pesquisa%'";
+  $resultado_pesquisa = mysqli_query($conn, $result_pesquisa);
+  while ($row_usuario = mysqli_fetch_assoc($resultado_pesquisa)){
+    echo("<br>--------------------------------------------------------------<br>");
+    echo " ID: ".$row_usuario['id_item']."<br>";
+    echo " Loja: ".$row_usuario['loja']."<br>";
+    echo " Código Identificador: ".$row_usuario['identificador']."<br>";
+    echo " Data de Registro: ".$row_usuario['data_compra']."<br>";
+    echo("--------------------------------------------------------------<br>");
+
+  }
+
+}
+
+
+//logoff da sistema
+if($_GET){
+  if($_GET['sair'] == 'sim'){
+    sair();
+  }
+}
+
