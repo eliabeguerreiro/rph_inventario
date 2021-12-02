03fd9c62a91e32827ca1fde1db671977e3700d47
Caio Souza <59893929+kcaiosouza@users.noreply.github.com>
2021-12-02 11:39:43 -0300

    Update-Style

--- index.php
+++ index.php
@@ -10,43 +10,41 @@ if(isset($_SESSION['msg'])){
 ?>
 
 <html>
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Login</title>
-  </head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Login | Inventário</title>
+</head>
+<body>
 
-  <body>
-
-    <div class="area-login">
-      <div class="text-container">
-        <img src="./images/logo.png" alt="Logo">
-        <h1 class="login-name">Área de Login</h1>
-        <h3>Efetue o seu login para prosseguir</h3>
-      </div>
-
-      <form method="POST" action="functions/loger.php">
-        <div class="login-container" >
-          <input placeholder="Login" name="login" required>
-        </div>
-        <br>
-        <div class="password-container">
-          <input placeholder="Senha" type="password" name="senha" required>
+    <div class="area-login shadow">
+        <div class="text-container">
+            <img src="./images/logo.png" alt="Logo">
+            <h1 class="login-name">Área de Login</h1>
+            <h3>Efetue o seu login para prosseguir</h3>
         </div>
 
-        <br>
+        <form method="POST" action="functions/loger.php">
+            <div class="login-container">
+                <input placeholder="Login" name="login" required>
+            </div>
+            <br>
+            <div class="password-container">
+                <input placeholder="Senha" type="password" name="senha" required>
+            </div>
 
-        <div class="button-container">
-          <button type="submit">Entrar</button>
-        </div>
+            <br>
 
-      </form>
+            <div class="button-container">
+                <button type="submit">Entrar</button>
+            </div>
+
+        </form>
 
     </div>
-
-  </body>  
+</body>
 </html>
-<?php
